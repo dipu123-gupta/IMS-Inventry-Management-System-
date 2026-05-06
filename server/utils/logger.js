84f00d64fd9ea103afda96e19bae488edb98f693
dipu123-gupta <kumardipu845420@gmail.com>
2026-05-06 11:08:54 +0530

    updatede

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -30,14 +30,13 @@ const logger = winston.createLogger({
   ],
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-      logFormat
-    ),
-  }));
-}
+// Always log to console so Render logs capture it
+logger.add(new winston.transports.Console({
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    logFormat
+  ),
+}));
 
 module.exports = logger;
